856e52052d5827f1da246afff69236aafb15ceaa
Robin-Ibn-Nur <robinibnnur@gmail.com>
2024-08-22 22:36:35 +0600

    second commit

---
+++ app/routes/app.collections.tsx
@@ -0,0 +1,93 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Card, Layout, List, Page } from "@shopify/polaris";
+import { apiVersion, authenticate } from "~/shopify.server";
+
+
+export const query =`
+{
+    collections(first: 10){
+        edges{
+            node{
+                id
+                handle
+                title
+                description
+            }
+        }
+        pageInfo {
+            hasNextPage
+        }
+    }
+}
+`
+
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { session } = await authenticate.admin(request)
+  const {shop, accessToken } = session;
+
+  try{
+
+      const response = await fetch(`https://${shop}/admin/api/${apiVersion}/graphql.json`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/graphql",
+              "X-Shopify-Access-Token": accessToken!
+          },
+          body: query
+
+      });
+
+      if(response.ok){
+          const data = await response.json()
+
+          const {
+              data: {
+                  collections: { edges }  
+              }
+          } = data;
+          return edges
+      }
+
+      return null
+
+  } catch(err){
+      console.log(err)
+  }
+
+}
+
+const Collections = () => {
+
+  const collections: any = useLoaderData()
+  console.log(collections, 'collections')
+
+  return (
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card><h1>Hello from collections</h1></Card>
+        </Layout.Section>
+        <Layout.Section>
+          <Card>
+            <List type="bullet" gap="loose">
+              {
+                collections.map((edge: any)=>{
+                  const {node: collection} =edge;
+                  return(<List.Item key={collection.id}>
+                    {collection.title}
+                    {collection.description}
+                  </List.Item>)
+                })
+              }
+            </List>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  )
+}
+
+export default Collections;
+
